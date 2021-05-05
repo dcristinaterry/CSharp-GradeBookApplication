b6d22bdcd725a62e3cb568d3dcdfcf8afd993261
Cristina Terry <cristina.terry@cir2.com>
2021-05-05 09:32:21 -0500

    add RankedGradeBooks

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,49 @@ namespace GradeBook.GradeBooks
             Type = Enums.GradeBookType.Ranked;
 
         }
+
+
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int numStudents = base.Students.Count;
+           
+            if (numStudents < 5) {
+
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+              
+            }
+
+            int counter = 0;
+            foreach (var student in base.Students) {
+
+                var studentAverageGrade = student.AverageGrade;
+
+                if (averageGrade > studentAverageGrade) {
+                    counter++;
+                }
+
+            }
+
+            int percentageGrade = (counter * 100) / numStudents;
+
+
+            if (percentageGrade > 80)
+                return 'F';
+            else if (percentageGrade > 60)
+                return 'D';
+            else if (percentageGrade > 40)
+                return 'C';
+            else if (percentageGrade > 20)
+                return 'B';
+            else
+                return 'A';
+
+          
+        }
+
     }
+
+
+
 }
